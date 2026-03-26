3a632c028d5bb8d787b6d271e4f1c605d64cef7d
NOIRET Maxime <maxime.noiret@etu.univ-amu.fr>
2026-03-26 20:15:46 +0100

    fix: Removed quotation marks from task title in display

--- task.c
+++ task.c
@@ -43,7 +43,7 @@ void task_create(sqlite3 *db, char *title, char *desc) {
 } // task_create
 
 void task_print(int id, const unsigned char *title, const unsigned char *desc) {
-  printf("%d - \"%s\"\n"
+  printf("%d - %s\n"
          "\t%s\n",
          id, title, desc);
 } // task_print
