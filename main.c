05a7a35a631836368df837bec28e9eb98f55a1d2
NOIRET Maxime <maxime.noiret@etu.univ-amu.fr>
2026-03-26 10:20:57 +0100

    dev: Switched to single letter commands for switch

--- main.c
+++ main.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(int argc, char **argv) {
   if (argc == 1) {
     fputs("Wrong usage.\n", stderr);
     fprintf(stderr, "Correct usage: %s [command].", argv[0]);
-    fprintf(stderr, "Commands available with %s help", argv[0]);
+    fprintf(stderr, "Commands available with %s help\n", argv[0]);
     return 1;
   }
-  if (strcmp(argv[1], "help") == 0) {
+  switch (argv[1][0]) {
+  case 'h':
+    puts("This is a ToDo list CLI utility written in C because uni is boring.\n"
+         "Only the first character of commands are taken into account.\n"
+         "  (Yes, 'cbloop' will count as 'create')\n");
     puts("Commands:\n"
-         "\tcreate [title]        Create a task.\n"
-         "\thelp                  Show this menu.");
+         "  c create (title) [description]    Create a task.\n"
+         "  g get [n]                         Show all or the nth last tasks.\n"
+         "  d delete (title)                  Delete task with title\n"
+         "  h help                            Show this menu.");
+    break;
+  case 'c':
+    // TODO: handle_create();
+    break;
+  case 'g':
+    // TODO: handle_get(int n);
+    break;
+  case 'd':
+    // TODO: handle_delete(char *title);
+    break;
+  default:
+    fputs("Unknown argument.\n", stderr);
+    fprintf(stderr, "Commands available with %s help\n", argv[0]);
+    return 1;
   }
-
   return 0;
 }
